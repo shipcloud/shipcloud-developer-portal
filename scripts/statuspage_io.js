2cf16ba1a4673b3e374df0a5c77114a7aa8c106b
André Cedik <andre.cedik@gmail.com>
2016-04-05 11:15:19 +0200

    added statuspage.io widget

---
+++ scripts/statuspage_io.js
@@ -0,0 +1,10 @@
+var sp = new StatusPage({ pageId: 'dlccblhllckf'});
+
+sp.getStatus({
+  success: function(data) {
+    // adds the text description to the dropdown
+    $('.color-description').text(data.status.description);
+    // appends the status indicator as a class name so we can use the right color for the status light thing
+    $('.color-dot').addClass(data.status.indicator);
+  }
+});
