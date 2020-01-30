da755a043db6929d788a24459804c0a877724e59
André Cedik <andre.cedik@gmail.com>
2020-01-30 19:33:37 +0100

    add Algolia DocSearch

---
+++ assets/js/algolia-docsearch.js
@@ -0,0 +1,6 @@
+docsearch({
+  apiKey: '2b4cb3f517296adace0e07dfd96ff6f1',
+  indexName: 'shipcloud',
+  inputSelector: '#search',
+  debug: false // Set debug to true if you want to inspect the dropdown
+});
