7aea71ca2737efa756a7a1f41441b1fb9d6b547d
André Cedik <andre@shipcloud.io>
2020-07-29 10:07:21 +0200

    added postcss plugin cssnano (#192)

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,9 @@ module.exports = {
     require("postcss-import"),
     require("tailwindcss")("./_includes/tailwind.config.js"),
     require("postcss-nested"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    require('cssnano')({
+      preset: 'default',
+    })
   ]
 }
