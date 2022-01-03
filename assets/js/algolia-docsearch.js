064b0719505b773bb6e16f40ded7023c0f06fe24
André Cedik <andre.cedik@gmail.com>
2022-01-03 08:56:59 +0100

    changed algolia configs since they've updated their service. See [their docs](https://docsearch.algolia.com/docs/migrating-from-legacy/) for more informatione

--- assets/js/algolia-docsearch.js
+++ assets/js/algolia-docsearch.js
@@ -1,5 +1,6 @@
 docsearch({
-  apiKey: '2b4cb3f517296adace0e07dfd96ff6f1',
+  apiKey: 'd1ac7e7af560dac9a33424fc98a37e06',
+  appId: "4LWVCBSAPI",
   indexName: 'shipcloud',
   inputSelector: '#search',
   debug: false // Set debug to true if you want to inspect the dropdown
